cb5143ac57d6f2998bb413c533e0bf83760352b9
Pedro Filho <pedro@filho.me>
2022-08-13 15:46:11 +0100

    fixing calculations

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -3,13 +3,26 @@ import { ApiPromise, WsProvider } from "@polkadot/api";
 import { web3Accounts, web3Enable } from "@polkadot/extension-dapp";
 import { InjectedAccountWithMeta } from "@polkadot/extension-inject/types";
 import { useEffect, useState } from "react";
-import { formatBalance } from "@polkadot/util";
+import { BN } from "@polkadot/util";
+import { Struct } from "@polkadot/types";
+import BigNumber from "bignumber.js";
 
-const RPC_PROVIDER = "wss://tinker.invarch.network/";
-// const RPC_PROVIDER = "wss://rpc.astar.network";
+// const RPC_PROVIDER = "wss://tinker.invarch.network/";
 
-// INVARCH const TEST_WALLET = "i4zTcKHr38MbSUrhFLVKHG5iULhYttBVrqVon2rv6iWcxQwQQ";
-// ASTAR const TEST_WALLET = "5HGabetgTWWsoRJxdZP11ns2yi1oVaenXEBprfKHp7tpw7NH";
+const RPC_PROVIDER = "wss://acala-rpc-0.aca-api.network";
+
+const TEST_WALLET = "265UKuZnACXcoYN7eXEEvxMSJBSPjiW8V8TdAnjezksDvpX5";
+
+// const TEST_WALLET = "i4zTcKHr38MbSUrhFLVKHG5iULhYttBVrqVon2rv6iWcxQwQQ";
+
+type SystemAccount = Struct & {
+  data: {
+    free: BN;
+    reserved: BN;
+    miscFrozen: BN;
+    feeFrozen: BN;
+  };
+};
 
 type NavigationProps = {
   className: string;
@@ -17,32 +30,6 @@ type NavigationProps = {
 };
 
 const navigation = [
-  {
-    name: "Facebook",
-    href: "#",
-    icon: (props: NavigationProps) => (
-      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-        <path
-          fillRule="evenodd"
-          d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-          clipRule="evenodd"
-        />
-      </svg>
-    ),
-  },
-  {
-    name: "Instagram",
-    href: "#",
-    icon: (props: NavigationProps) => (
-      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-        <path
-          fillRule="evenodd"
-          d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-          clipRule="evenodd"
-        />
-      </svg>
-    ),
-  },
   {
     name: "Twitter",
     href: "#",
@@ -69,6 +56,7 @@ const navigation = [
 
 const Home = () => {
   const [account, setAccount] = useState<InjectedAccountWithMeta | null>(null);
+  const [claimData, setClaimData] = useState<any>(null);
 
   const handleConnect = async () => {
     const extensions = await web3Enable("InvArch Tinker Network");
@@ -86,53 +74,85 @@ const Home = () => {
     setAccount(allAccounts[0]);
   };
 
-  const loadBalances = async (account: InjectedAccountWithMeta) => {
+  const loadBalances = async ({ address }: InjectedAccountWithMeta) => {
     const wsProvider = new WsProvider(RPC_PROVIDER);
 
     const api = await ApiPromise.create({ provider: wsProvider });
 
-    const {
-      data: { free, reserved, miscFrozen, feeFrozen },
-      nonce,
-    } = await api.query.system.account(account.address);
+    const results = await Promise.all([
+      // vested locked
+      api.query.balances.locks(/* address */ TEST_WALLET),
+      // vesting schedules
+      api.query.vesting.vestingSchedules(/* address */ TEST_WALLET),
+      // current block
+      api.query.system.number(),
+      // total
+      api.query.system.account<SystemAccount>(/* address */ TEST_WALLET),
+    ]);
 
-    const locks = await api.query.balances.locks(account.address);
+    const vestedLocked = new BigNumber(
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      results[0].find((lock) => lock.id.toHuman() === "ormlvest")?.amount || "0"
+    );
 
-    console.log(
-      locks.map((lock) => ({
-        id: lock.id.toString(),
-        amount: formatBalance(lock.amount, {
-          decimals: 12,
-          withSi: false,
-          forceUnit: "-",
-        }),
-        reasons: lock.reasons.toString(),
-      }))
+    const vestingSchedules = results[1] as any;
+
+    const currentBlock = results[2].toHuman();
+
+    const sumFutureLock = vestingSchedules.reduce(
+      (acc: any, vestingSchedule: any) => {
+        const startPeriod = new BigNumber(vestingSchedule.start);
+
+        const period = new BigNumber(vestingSchedule.period);
+
+        // if the vesting has not started, number of periods is 0
+        let numberOfPeriods = new BigNumber(currentBlock)
+          .minus(startPeriod)
+          .dividedBy(period);
+
+        numberOfPeriods = numberOfPeriods.isNegative()
+          ? new BigNumber("0")
+          : numberOfPeriods;
+
+        const perPeriod = new BigNumber(vestingSchedule.perPeriod);
+
+        const vestedOverPeriods = numberOfPeriods.multipliedBy(perPeriod);
+
+        const periodCount = new BigNumber(vestingSchedule.periodCount);
+
+        const originalLock = periodCount.multipliedBy(perPeriod);
+
+        const unlocked = vestedOverPeriods.gte(originalLock)
+          ? originalLock
+          : vestedOverPeriods;
+
+        const futureLock = originalLock.minus(unlocked);
+
+        return acc.plus(futureLock);
+      },
+      new BigNumber("0")
     );
 
     console.log({
-      nonce: nonce.toNumber(),
-      free: formatBalance(free, {
-        decimals: 12,
-        withSi: false,
-        forceUnit: "-",
-      }),
-      reserved: formatBalance(reserved, {
-        decimals: 12,
-        withSi: false,
-        forceUnit: "-",
-      }),
-      miscFrozen: formatBalance(miscFrozen, {
-        decimals: 12,
-        withSi: false,
-        forceUnit: "-",
-      }),
-      feeFrozen: formatBalance(feeFrozen, {
-        decimals: 12,
-        withSi: false,
-        forceUnit: "-",
-      }),
-      locks,
+      sumFutureLock: sumFutureLock.toString(),
+      vestedLocked: vestedLocked.toString(),
+      vestedLockedMinusSumFutureLock: vestedLocked
+        .minus(sumFutureLock)
+        .toString(),
+    });
+
+    const vestedClaimable = vestedLocked.minus(sumFutureLock);
+
+    const total = new BigNumber(results[3].data.free.toString());
+
+    const transferable = total.minus(vestedLocked);
+
+    console.log({
+      vestedLocked: vestedLocked.toString(),
+      vestedClaimable: vestedClaimable.toString(),
+      total: total.toString(),
+      transferable: transferable.toString(),
     });
   };
 
