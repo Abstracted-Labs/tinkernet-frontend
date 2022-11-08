4f5876d7e0fcad20884655a6749c6bad41ea0d92
Gabriel Facco de Arruda <arrudagates@gmail.com>
2022-11-08 14:08:57 -0300

    Update xtransfer.tsx

--- src/routes/xtransfer.tsx
+++ src/routes/xtransfer.tsx
@@ -434,11 +434,11 @@ const XTransfer = () => {
                       <button
                         type="button"
                         className="inline-flex items-center justify-center rounded-md border border-amber-300 bg-amber-300 px-4 py-2 text-base font-medium text-black shadow-sm focus:outline-none focus:ring-2 focus:ring-neutral-500 focus:ring-offset-2 disabled:opacity-75"
-                        disabled={
-                          new BigNumber(amount) >=
-                            balanceInTinkernet.minus(100000000000) ||
-                          !destination
-                        }
+                        disabled={true}//{
+                          //new BigNumber(amount) >=
+                          //  balanceInTinkernet.minus(100000000000) ||
+                          //!destination
+                        //}
                         onClick={() => handleXTransferToBasilisk()}
                       >
                         Transfer
