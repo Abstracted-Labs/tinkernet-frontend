d4a605f9364661b18e5356a3bebad509190d03b3
Gabriel Facco de Arruda <arrudagates@gmail.com>
2023-01-03 12:24:50 -0300

    Some minor changes

--- src/routes/staking.tsx
+++ src/routes/staking.tsx
@@ -372,7 +372,7 @@ const Staking = () => {
                     type="button"
                     className="inline-flex items-center justify-center rounded-md bg-amber-300 px-4 py-2 text-base font-medium text-black shadow-sm hover:bg-amber-200 focus:outline-none focus:ring-2 focus:ring-neutral-500 focus:ring-offset-2 disabled:bg-neutral-400"
                     onClick={handleClaimAll}
-                    // disabled={unclaimedEras.total === 0}
+                    disabled={unclaimedEras.total === 0}
                   >
                     Claim All
                   </button>
@@ -382,7 +382,7 @@ const Staking = () => {
               <div className="relative overflow-hidden rounded-md border border-neutral-50 bg-neutral-900 shadow sm:grid md:grid-cols-2 lg:grid-cols-4">
                 <div className="flex flex-col gap-2 p-6">
                   <div>
-                    <span className="text-sm">Total staked</span>
+                    <span className="text-sm">Your stake</span>
                   </div>
                   <div>
                     <span className="text-2xl font-bold">
@@ -390,7 +390,7 @@ const Staking = () => {
                         decimals: 12,
                         withUnit: false,
                         forceUnit: "-",
-                      }).slice(0, -2)}{" "}
+                      }).slice(0, -2) || "0"}{" "}
                       TNKR
                     </span>
                   </div>
@@ -417,7 +417,7 @@ const Staking = () => {
                         decimals: 12,
                         withUnit: false,
                         forceUnit: "-",
-                      }).slice(0, -2)}{" "}
+                      }).slice(0, -2) || "0"}{" "}
                       TNKR
                     </span>
                   </div>
@@ -498,7 +498,7 @@ const Staking = () => {
 
                           <span className="block text-sm">
                             {totalStaked
-                              ? `Staked ${formatBalance(
+                              ? `Your stake: ${formatBalance(
                                   totalStaked.toString(),
                                   {
                                     decimals: 12,
