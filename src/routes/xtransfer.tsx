441252ba22ed56a29e846f9048c22e0de68ce9f0
Gabriel Facco de Arruda <arrudagates@gmail.com>
2023-04-02 21:07:16 -0400

    fix: Fix amount field

--- src/routes/xtransfer.tsx
+++ src/routes/xtransfer.tsx
@@ -369,8 +369,8 @@ const XTransfer = () => {
                     type="button"
                     className="inline-flex items-center justify-center rounded-md border border-amber-300 bg-amber-300 px-4 py-2 text-base font-medium text-black shadow-sm focus:outline-none focus:ring-2 focus:ring-neutral-500 focus:ring-offset-2 disabled:opacity-75"
                     disabled={
-                      new BigNumber(amount) >=
-                        balanceInTinkernet.minus(100000000000) || !destination
+                      (new BigNumber(amount).div(1000000000000).toNumber() >=
+                        balanceInTinkernet.minus(100000000000).div(1000000000000).toNumber()) || !destination
                     }
                     onClick={() => handleXTransferToBasilisk()}
                   >
@@ -436,8 +436,8 @@ const XTransfer = () => {
                     type="button"
                     className="inline-flex items-center justify-center rounded-md border border-amber-300 bg-amber-300 px-4 py-2 text-base font-medium text-black shadow-sm focus:outline-none focus:ring-2 focus:ring-neutral-500 focus:ring-offset-2 disabled:opacity-75"
                     disabled={
-                      new BigNumber(amount) >=
-                        balanceInBasilisk.minus(100000000000) || !destination
+                      new BigNumber(amount).div(1000000000000).toNumber() >=
+                        balanceInBasilisk.minus(100000000000).div(1000000000000).toNumber() || !destination
                     }
                     onClick={() => handleXTransferToTinkernet()}
                   >
