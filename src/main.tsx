80d5e2ecc563456a4b3f55e40f82e2d69854fee1
Yaki (Shiba Tales) <83098539+shibatales@users.noreply.github.com>
2023-12-16 13:43:24 -0800

    Update squids in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -22,11 +22,11 @@ import Layout from "./components/Layout";
 import Modals from "./modals";
 
 const wsClient = createWSClient({
-  url: "wss://squid.subsquid.io/ocif-squid/v/v1/graphql",
+  url: "wss://squid.subsquid.io/ocif-squid/v/v3/graphql",
 });
 
 const client = new Client({
-  url: "https://squid.subsquid.io/ocif-squid/v/v1/graphql",
+  url: "https://squid.subsquid.io/ocif-squid/v/v3/graphql",
   exchanges: [
     cacheExchange,
     fetchExchange,
