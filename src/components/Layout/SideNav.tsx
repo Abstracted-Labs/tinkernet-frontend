5392465414c606c9014eee1fd39ed32165fef320
Yaki <shibatales@gmail.com>
2024-01-09 05:33:22 -0800

    Add missing dependency cleanup

--- src/components/Layout/SideNav.tsx
+++ src/components/Layout/SideNav.tsx
@@ -33,6 +33,7 @@ const SideNav = (props: SideNavProps) => {
     return () => {
       if (navOpen) navOpen(false);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
