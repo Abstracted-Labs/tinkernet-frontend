e871d72f82835995a4b2649ddc5bd65d20e86407
Yaki <shibatales@gmail.com>
2024-01-11 11:53:36 -0800

    Update server.js with new port and host configuration

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ import express from 'express';
 import { getAssetsData } from './apiUtils.cjs';
 
 const app = express();
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 7777;
+let host;
+
+if (process.env.NODE_ENV === 'production') {
+  host = 'https://tinker.network';
+} else {
+  host = 'http://localhost';
+}
 
 app.get('/api/cg/tickers', async (req, res) => {
   const assetsData = await getAssetsData();
@@ -10,5 +17,5 @@ app.get('/api/cg/tickers', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${ port }`);
+  console.log(`Server is running at ${ host }:${ port }`);
 });
