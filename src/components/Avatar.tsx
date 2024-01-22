a0c4eaf8a4a047f1e501691e31daf5a69a8f809d
Yaki <shibatales@gmail.com>
2024-01-21 20:33:16 -0800

    Add new SVG icons and update Avatar component

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,26 +1,28 @@
-import React, { useState } from 'react';
+import React, { memo, useState } from 'react';
 
 interface AvatarProps {
-  src: string;
+  src: string | undefined;
   alt: string;
+  mini?: boolean;
 }
 
-const Avatar: React.FC<AvatarProps> = ({ src, alt }) => {
+const Avatar = (props: AvatarProps) => {
+  const { src, alt, mini } = props;
   const [hasError, setHasError] = useState(false);
 
   const handleError = () => {
     setHasError(true);
   };
-
+  console.log('Avatar', src);
   return (
     <div className="flex flex-shrink-0">
-      {hasError ? (
-        <div className="h-16 w-16 rounded-full animate-colorChange" />
+      {src === undefined || hasError ? (
+        <div className={`${ mini ? 'h-10 w-10' : 'h-16 w-16' } rounded-full animate-colorChange`} />
       ) : (
         <img
           src={src}
           alt={alt}
-          className="h-16 w-16 rounded-full"
+          className={`${ mini ? 'h-10 w-10' : 'h-16 w-16' } rounded-full`}
           onError={handleError}
         />
       )}
@@ -28,4 +30,4 @@ const Avatar: React.FC<AvatarProps> = ({ src, alt }) => {
   );
 };
 
-export default Avatar;
+export default memo(Avatar);
